41de2429614af97a87c827e826e30adbfe3501d6
pkurjanowicz <pkurjanowicz5@gmail.com>
2019-05-04 15:14:04 -0600

    Update get_country_codes.py

--- get_country_codes.py
+++ get_country_codes.py
@@ -1,3 +1,16 @@
-def get_country_codes(param):
-    # Your code here. Maybe try pseudocoding this one out?
-    return ""
+def get_country_codes(prices):
+    split_string = prices.split()
+    final_str = ''
+    for country in split_string:
+        new_value = country[:2]
+        new_country_str = new_value + ", "
+        final_str += new_country_str
+    final_str = final_str[:-2]
+    return final_str
+
+from test import testEqual
+
+testEqual(get_country_codes("NZ$300, KR$1200, DK$5"), "NZ, KR, DK")
+testEqual(get_country_codes("US$40, AU$89, JP$200"), "US, AU, JP")
+testEqual(get_country_codes("AU$23, NG$900, MX$200, BG$790, ES$2"), "AU, NG, MX, BG, ES")
+testEqual(get_country_codes("CA$40"), "CA")
