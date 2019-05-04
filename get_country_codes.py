c510d6b78193ea287ff156ad9282ffcff0661cda
pkurjanowicz <pkurjanowicz5@gmail.com>
2019-05-04 15:30:25 -0600

    Update get_country_codes.py

--- get_country_codes.py
+++ get_country_codes.py
@@ -7,10 +7,3 @@ def get_country_codes(prices):
         final_str += new_country_str
     final_str = final_str[:-2]
     return final_str
-
-from test import testEqual
-
-testEqual(get_country_codes("NZ$300, KR$1200, DK$5"), "NZ, KR, DK")
-testEqual(get_country_codes("US$40, AU$89, JP$200"), "US, AU, JP")
-testEqual(get_country_codes("AU$23, NG$900, MX$200, BG$790, ES$2"), "AU, NG, MX, BG, ES")
-testEqual(get_country_codes("CA$40"), "CA")
